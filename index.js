c731330c054a5853a38574820f01934b510a3f3b
ilyosbek <ilyosbek.inomjonov.1996@gmail.com>
2021-12-09 10:42:33 +0500

    Done

---
+++ index.js
@@ -0,0 +1,14 @@
+var a = 2,
+b = a * a * a;
+console.log(b);
+var a = 2**3;
+console.log(a);
+var a = 81,
+b = a ** 0.5;
+console.log(b);
+var a = 81 ** 0.5;
+console.log(a);
+var pi = Math.PI,
+    a = 1;
+console.log(pi.toFixed());
+console.log(+pi.toFixed() + 1);
